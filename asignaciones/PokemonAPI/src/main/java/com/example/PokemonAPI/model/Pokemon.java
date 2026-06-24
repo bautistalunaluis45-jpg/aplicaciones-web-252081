6fe59a39d31e0dfc35387b8273c07508beaf2a47
Luis Bautista <luisb@2806-1016-000a-860a-0000-0000-0000-0009.ipv6.infinitum.net.mx>
2026-06-24 14:36:40 -0700

    Practica

---
+++ asignaciones/PokemonAPI/src/main/java/com/example/PokemonAPI/model/Pokemon.java
@@ -0,0 +1,45 @@
+package com.example.PokemonAPI.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String nombre;
+
+
+    private String tipo;
+
+
+    private Integer nivel;
+
+
+    private Integer ataque;
+
+
+    private Integer defensa;
+
+
+    private String descripcion;
+
+
+    private String imagnUrl;
+
+
+
+
+}
